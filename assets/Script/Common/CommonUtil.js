fdb6f3763d79e6326dc97dd9cbee0a651493582f
张航 <zhanghang@digisky.com>
2020-04-29 17:32:07 +0800

    添加一个UTIL

---
+++ assets/Script/Common/CommonUtil.js
@@ -0,0 +1,20 @@
+/*
+    场景中的物体的类型
+*/
+var EObjType = {
+    TYPE_ROAD   :   0,
+    TYPE_BARRIER:   1,
+    TYPE_ENEMY  :   2,
+    TYPE_ITEM   :   3,
+
+};
+
+
+
+
+
+
+
+module.exports = {
+    EObjType = EObjType,
+};
